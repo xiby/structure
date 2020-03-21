fc6cd799403a49b43ae4235ce26746746490a197
xiby <15555438336@163.com>
2020-03-21 17:20:05 +0800

    [修改代码](排序)添加快速排序实现

--- src/main/java/tech/xiby/structure/sort/Sorter.java
+++ src/main/java/tech/xiby/structure/sort/Sorter.java
@@ -24,4 +24,47 @@ public class Sorter {
             }
         }
     }
+
+    /**
+     * 快速排序
+     * 利用递归实现
+     *
+     * @param datas
+     * @param comparator
+     * @param <T>
+     */
+    public static <T> void quickSort(T[] datas, IComparator<T> comparator) {
+        if (datas.length == 0) {
+            return;
+        }
+        doQuickSort(datas, 0, datas.length - 1, comparator);
+    }
+
+    private static <T> void doQuickSort(T[] datas, int start, int end, IComparator<T> comparator) {
+        if (start >= end) {
+            return;
+        }
+        T pivot = datas[start];
+        int low = start;
+        int high = end;
+        int pivotPosition = start;
+        T tmp = null;
+        while (low < high) {
+            while (low < high && comparator.compair(datas[high], pivot)) {
+                --high;
+            }
+            tmp = datas[low];
+            datas[low] = datas[high];
+            datas[high] = tmp;
+            while (low < high && comparator.compair(pivot, datas[low])) {
+                ++low;
+            }
+            tmp = datas[high];
+            datas[high] = datas[low];
+            datas[low] = tmp;
+            pivotPosition = low;
+        }
+        doQuickSort(datas, start, pivotPosition - 1, comparator);
+        doQuickSort(datas, pivotPosition + 1, end, comparator);
+    }
 }
--- src/test/java/tech/xiby/structure/sort/SorterTest.java
+++ src/test/java/tech/xiby/structure/sort/SorterTest.java
@@ -2,6 +2,8 @@ package tech.xiby.structure.sort;
 
 import org.junit.Test;
 
+import java.util.Random;
+
 public class SorterTest {
 
     @Test
@@ -11,5 +13,51 @@ public class SorterTest {
         for (int i = 0; i < nums.length; ++i) {
             System.out.println(nums[i]);
         }
+    }
+
+    @Test
+    public void quickSort() {
+        Integer[] nums = new Integer[10];
+        for (int i = 0; i < 10; ++i) {
+            nums[i] = new Random().nextInt(100);
+        }
+        Sorter.quickSort(nums, ((num1, num2) -> num1 >= num2));
+        for (int i = 0; i < nums.length; ++i) {
+            System.out.println(nums[i]);
+        }
+    }
+
+    @Test
+    public void compair() {
+        int count = 1000;
+        //对100组数据进行排序
+        Integer[][] datas1 = new Integer[count][];
+        Integer[][] datas2 = new Integer[count][];
+        //排序100次
+        int size = 1000;
+
+        //首先构造数据
+        for (int i = 0; i < count; ++i) {
+            datas1[i] = new Integer[size];
+            datas2[i] = new Integer[size];
+            for (int j = 0; j < size; ++j) {
+                Integer num = new Random().nextInt(size * 10);
+                datas1[i][j] = num;
+                datas2[i][j] = num;
+            }
+        }
+        System.out.println("结束构造");
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < count; ++i) {
+            Sorter.bubbleSort(datas1[i], ((num1, num2) -> num1 >= num2));
+        }
+        System.out.println("冒泡排序耗时：" + (System.currentTimeMillis() - start) + " 毫秒");
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < count; ++i) {
+            Sorter.quickSort(datas2[i], ((num1, num2) -> num1 >= num2));
+        }
+        System.out.println("冒泡排序耗时：" + (System.currentTimeMillis() - start) + " 毫秒");
     }
 }
